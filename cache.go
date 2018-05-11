80e636f7ef50a1954e1a1552fe87a2470b817e1a
Fabian Ruff <fabian@progra.de>
2018-05-11 14:53:13 +0200

    initial commit

---
+++ cache.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/majewsky/schwift"
+	"github.com/majewsky/schwift/gopherschwift"
+)
+
+type Cache interface {
+	Get(key string) (*url.URL, bool, error)
+	Store(key string, object io.Reader) (*url.URL, error)
+}
+
+type LocalCache struct {
+	BaseURL  string
+	CacheDir string
+}
+
+func (l *LocalCache) Get(key string) (*url.URL, bool, error) {
+
+	path := filepath.Join(l.CacheDir, key)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, false, nil
+	}
+	url, err := url.Parse(l.BaseURL + key)
+	return url, true, err
+}
+
+func (l *LocalCache) Store(key string, object io.Reader) (*url.URL, error) {
+	path := filepath.Join(l.CacheDir, key)
+
+	url, err := url.Parse(l.BaseURL + key)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return nil, err
+	}
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, object)
+	return url, err
+
+}
+
+type SwiftCache struct {
+	container *schwift.Container
+}
+
+func NewSwiftCache(container string) (*SwiftCache, error) {
+
+	if container == "" {
+		return nil, errors.New("Container name required")
+	}
+	authOptions, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		return nil, err
+	}
+	authOptions.AllowReauth = true
+	provider, err := openstack.AuthenticatedClient(authOptions)
+	if err != nil {
+		return nil, err
+	}
+	client, err := openstack.NewObjectStorageV1(provider, gophercloud.EndpointOpts{})
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := gopherschwift.Wrap(client, nil)
+	if err != nil {
+		return nil, err
+	}
+	c, err := account.Container(container).EnsureExists()
+	if err != nil {
+		return nil, err
+	}
+	return &SwiftCache{container: c}, nil
+}
+
+func (s *SwiftCache) Get(key string) (*url.URL, bool, error) {
+	key = strings.TrimLeft(key, "/")
+	object := s.container.Object(key)
+	exists, err := object.Exists()
+	if err != nil {
+		return nil, false, err
+	}
+	if exists {
+		objectURL, err := object.URL()
+		if err != nil {
+			return nil, false, err
+		}
+		u, err := url.Parse(objectURL)
+		return u, true, err
+	}
+	return nil, false, nil
+}
+
+func (s *SwiftCache) Store(key string, data io.Reader) (*url.URL, error) {
+
+	key = strings.TrimLeft(key, "/")
+
+	object := s.container.Object(key)
+
+	if err := object.Upload(data, nil, nil); err != nil {
+		return nil, err
+	}
+	objectURL, err := object.URL()
+	if err != nil {
+		return nil, err
+	}
+	return url.Parse(objectURL)
+
+}
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/im7mortal/kmutex"
+)
+
+var (
+	omnitruckURL     string
+	cacheBackendName string
+)
+
+type OmnitruckProxy struct {
+	Cache      Cache
+	client     http.Client
+	BackendURL string
+	mutex      *kmutex.Kmutex
+}
+
+type OmnitruckResponse struct {
+	Url     string `json:"url"`
+	Sha256  string `json:"sha256"`
+	Sha1    string `json:"sha1"`
+	Version string `json:"version"`
+}
+
+func init() {
+	flag.StringVar(&omnitruckURL, "omnitruck-url", "https://omnitruck.chef.io", "backend omnitruck url")
+	flag.StringVar(&cacheBackendName, "cache-backend", "local", "which cache backend to use")
+}
+
+func main() {
+
+	flag.Parse()
+
+	var cacheBackend Cache
+	switch cacheBackendName {
+	case "local":
+		localCache := &LocalCache{BaseURL: "http://localhost:8080/packages", CacheDir: "cache"}
+		http.Handle("/packages/", http.StripPrefix("/packages/", http.FileServer(http.Dir(localCache.CacheDir))))
+		cacheBackend = localCache
+	case "swift":
+		var err error
+		cacheBackend, err = NewSwiftCache(os.Getenv("OS_CONTAINER"))
+		if err != nil {
+			log.Fatalf("Failed to initialize swift backend: %s", err)
+		}
+	default:
+		log.Fatalf("unknown cache backend: %s", cacheBackendName)
+	}
+
+	log.Printf("Using %s cache backend", cacheBackendName)
+
+	proxy := OmnitruckProxy{Cache: cacheBackend, BackendURL: omnitruckURL, mutex: kmutex.New()}
+	http.Handle("/", &proxy)
+
+	listen := ":8080"
+	log.Printf("Listening on %s", listen)
+	log.Fatal(http.ListenAndServe(listen, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux)))
+}
+
+func (o *OmnitruckProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	response, err := o.proxy(req)
+
+	if req.Header.Get("Accept") == "application/json" {
+		rw.Header().Set("Content-Type", "application/json")
+		if err == nil {
+			json.NewEncoder(rw).Encode(response)
+
+		} else {
+			rw.WriteHeader(500)
+			msg := map[string]string{"error": err.Error()}
+			json.NewEncoder(rw).Encode(msg)
+		}
+	} else {
+		rw.Header().Set("Content-Type", "text/plain")
+		if err == nil {
+			rw.Write([]byte(fmt.Sprintf(
+				"sha1\t%s\nsha256\t%s\nurl\t%s\nversion\t%s\n",
+				response.Sha1,
+				response.Sha256,
+				response.Url,
+				response.Version,
+			)))
+		} else {
+			rw.WriteHeader(500)
+			rw.Write([]byte(err.Error()))
+		}
+	}
+
+}
+
+func (o *OmnitruckProxy) proxy(req *http.Request) (*OmnitruckResponse, error) {
+	backendURL := fmt.Sprintf("%s%s", o.BackendURL, req.RequestURI)
+
+	backendReq, err := http.NewRequest(req.Method, backendURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create backend request: %v", err)
+	}
+	backendReq.Header.Set("Accept", "application/json")
+
+	response, err := o.client.Do(backendReq)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to perform backend request: %v", err)
+	}
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read backend response: %v", err)
+	}
+	if response.StatusCode >= 400 {
+		return nil, fmt.Errorf("Backend responded with %s: %s", response.Status, []byte(responseBytes))
+	}
+	var omni OmnitruckResponse
+	if err := json.Unmarshal(responseBytes, &omni); err != nil {
+		return nil, fmt.Errorf("Failed to parse backend response. response: %s, error: %v", string(responseBytes), err)
+	}
+
+	packageURL, err := url.Parse(omni.Url)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse package url: %s, error: %v", omni.Url, err)
+	}
+
+	key := packageURL.EscapedPath()
+	o.mutex.Lock(key)
+	defer o.mutex.Unlock(key)
+	cacheUrl, found, err := o.Cache.Get(packageURL.EscapedPath())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to query the cache: %v", err)
+	}
+	if !found {
+		log.Printf("Caching %s", omni.Url)
+		resp, err := o.client.Get(omni.Url)
+		if err != nil {
+			return nil, fmt.Errorf("Fetching %s failed: %v", omni.Url, err)
+		}
+		defer resp.Body.Close()
+
+		cacheUrl, err = o.Cache.Store(packageURL.EscapedPath(), resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to store %s in cache: %v ", omni.Url, err)
+		}
+	}
+	omni.Url = cacheUrl.String()
+	return &omni, nil
+}
